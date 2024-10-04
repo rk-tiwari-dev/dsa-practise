c998a690054e5b497113192bd7aa305e860d45bd
rk-tiwari-dev <rkgihub01gmail.com>
2024-10-04 16:14:01 +0530

    minor

--- LinkedList/addTwoNumberGivenByALL.py
+++ LinkedList/addTwoNumberGivenByALL.py
@@ -1,4 +1,7 @@
-from crio.ds.List.ListNode import *
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 def reverseList(head: ListNode) -> ListNode:
